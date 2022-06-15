178872840ca0bd768a6607df5151d00f72b62139
Junaidhassan99 <61021884+Junaidhassan99@users.noreply.github.com>
2022-06-15 08:29:16 +0500

    updates

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     return [
       {
         source: '/',
-        destination: '/buyer',
+        destination: '/choice',
         permanent: true,
       },
     ]
---
+++ pages/choice.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import Card from "../components/card";
+
+const HomeScreen = () => {
+  return (
+    <div>
+      <div className="grid place-items-center h-screen">
+        <Card additionClasses="px-10">
+          <div className="flex flex-col items-center">
+            <div className="p-3 font-bold text-xl">Login As</div>
+            <div className="p-3">
+              <Link href="/buyer/auth/login">
+                <button className="primary-color py-1 px-24 rounded-lg">
+                  <a>Buyer</a>
+                </button>
+              </Link>
+            </div>
+            <div className="p-3">- Or -</div>
+            <div className="p-3">
+              <Link href="/seller/auth/login">
+                <button className="primary-color py-1 px-24 rounded-lg">
+                  <a>Seller</a>
+                </button>
+              </Link>
+            </div>
+          </div>
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export default HomeScreen;
